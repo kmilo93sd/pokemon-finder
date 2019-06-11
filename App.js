d1333d79aef3a6c6148e1def57aa75a37c7faf63
Camilo Alaniz <kmilo93sd@gmail.com>
2019-06-11 19:54:42 -0400

    showing pokemon data on screen

--- App.js
+++ App.js
@@ -13,27 +13,88 @@ import {
   Text,
   View,
   TouchableHighlight,
+  Image,
 } from 'react-native';
+import axios from "axios";
 
 const message = "Bienvenido a PokeFinder";
 
-const handlePress = () => {
-  alert("WTF nigga")
+const getPokemonByName = async (pokemonToFind) => {
+
+  const endpoint = 'https://pokeapi.co/api/v2/pokemon/' + pokemonToFind.toLowerCase();
+
+  return await axios.get(endpoint)
+      .then(
+          response => {
+            return response.data
+          }
+      )
+      .catch(
+          error => {
+            return error
+          }
+      );
 };
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      pokemonToFind: '',
+      pokemon: {}
+    };
+  }
+
+  handlePress = async () => {
+    if (!this.state.pokemonToFind) {
+      alert("WTF nigga, write the fucking pokimom!")
+      return;
+    }
+
+    const pokemonFound = await getPokemonByName(this.state.pokemonToFind)
+        .then( data => {
+            return data;
+        })
+        .catch( error => {
+          alert(error)
+        });
+
+      this.setState({
+          pokemon: {
+              id: pokemonFound.id,
+              name: pokemonFound.name,
+              height: (pokemonFound.height * 10)/100 + ' m',
+              weight: (pokemonFound.weight * 10)/1000 + ' kg',
+              type: pokemonFound.types[0].type.name,
+              image: pokemonFound.sprites.front_default
+          }
+      });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>{ message }</Text>
-        <TextInput placeholder="¿Que pokemon buscas?" />
+        <TextInput
+            placeholder="¿Que pokemon buscas?"
+            onChangeText={(pokemonToFind) => this.setState({pokemonToFind})}
+        />
         <TouchableHighlight
             style={styles.button}
-            onPress={handlePress}
+            onPress={this.handlePress}
         >
           <Text style={[styles.buttonFont]}>Buscar</Text>
         </TouchableHighlight>
+          <Text>{ 'Número: ' + this.state.pokemon.id }</Text>
+        <Text>{ 'Nombre: ' + this.state.pokemon.name }</Text>
+         <Text>{ 'Altura: ' + this.state.pokemon.height}</Text>
+         <Text>{ 'Peso: ' + this.state.pokemon.weight }</Text>
+         <Text>{ 'Tipo: ' + this.state.pokemon.type }</Text>
+         <Image
+             style={{width: 200, height: 200}}
+             source={ {uri: this.state.pokemon.image }}
+         />
       </View>
     );
   }
